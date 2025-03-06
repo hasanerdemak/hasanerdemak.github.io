59b38870f54257b4457b8b0db200c35af0223a34
Hasan Erdem AK <hasanerdemak@gmail.com>
2025-03-06 06:25:23 +0300

    assets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={"/assets/images/hasan-erdem-ak.jpg"}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
